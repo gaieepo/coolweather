e4b0c96274de36913ada7500721d6ebc43c6a9f4
gaieepo <gaieepo@gmail.com>
2016-05-26 15:06:54 +0800

    Add db helper and classes correspond to respective tables

---
+++ app/src/main/java/com/example/coolweather/app/model/City.java
@@ -0,0 +1,43 @@
+package com.example.coolweather.app.model;
+
+/**
+ * Created by gaieepo on 26/5/2016.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
