d14df3c9a3380778a761543ee9bfb5438167a2b6
gaieepo <gaieepo@gmail.com>
2016-07-03 20:19:11 +0800

    fix broadcast receiver

--- app/src/main/java/com/example/coolweather/app/service/AutoUpdateService.java
+++ app/src/main/java/com/example/coolweather/app/service/AutoUpdateService.java
@@ -9,6 +9,7 @@ import android.os.IBinder;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import com.example.coolweather.app.receiver.AutoUpdateReceiver;
 import com.example.coolweather.app.util.HttpCallbackListener;
 import com.example.coolweather.app.util.HttpUtil;
 import com.example.coolweather.app.util.Utility;
@@ -34,7 +35,7 @@ public class AutoUpdateService extends Service {
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         int anHour = 8 * 60 * 60 * 1000;
         long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
-        Intent i = new Intent(this, AutoUpdateService.class);
+        Intent i = new Intent(this, AutoUpdateReceiver.class);
         PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
         manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
         return super.onStartCommand(intent, flags, startId);
